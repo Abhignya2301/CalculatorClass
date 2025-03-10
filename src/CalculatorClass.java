e1761aea27817a6446d96d85cd6e39af6c6ae809
abhignya <abhignya.ab@gmail.com>
2025-03-09 22:35:15 -0500

    "Removed main method from calculator class"

--- src/CalculatorClass.java
+++ src/CalculatorClass.java
@@ -1,17 +1,5 @@
 public class CalculatorClass {
-    public static void main(String[] args) {
-        CalculatorClass a = new CalculatorClass();
-        System.out.println(a.add(2,3));
-        CalculatorClass b = new CalculatorClass();
-        System.out.println(b.multiply(3,4));
-        CalculatorClass c = new CalculatorClass();
-        System.out.println(c.divide(3,4));
-        CalculatorClass d = new CalculatorClass();
-        System.out.println(d.subtract(3,4));
 
-
-
-    }
     public int multiply(int a, int b) {
         return a * b;
     }
